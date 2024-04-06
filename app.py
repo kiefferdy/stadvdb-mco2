d0603a3b13410b341a1dd2fe15ace15ebe4b1879
kiefferdy <kieffer_recatody@dlsu.edu.ph>
2024-04-07 01:38:41 +0800

    Basic frontend

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('views/index.html')
+
+@app.route('/appointments')
+def appointments():
+    return render_template('views/appointments.html')
+
+@app.route('/doctors')
+def doctors():
+    return render_template('views/doctors.html')
+
+@app.route('/patients')
+def patients():
+    return render_template('views/patients.html')
+
+@app.route('/clinics')
+def clinics():
+    return render_template('views/clinics.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
