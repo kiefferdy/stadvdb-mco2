883bfde53d34c95c76108fa01e35a7269e93b0da
Timothy Tan <98691592+Mitothy@users.noreply.github.com>
2024-04-08 20:45:40 +0800

    Appointments Dropdown

--- app.py
+++ app.py
@@ -25,26 +25,34 @@ def appointments():
     engine = sa.create_engine(f"mysql+mysqldb://{USERNAME}:{PASSWORD}@ccscloud.dlsu.edu.ph:{PORT}/",
                               isolation_level="READ COMMITTED")
 
-    stmt = sa.text(f"SELECT *\nFROM {SCHEMA}.{TABLE}\nWHERE pxid = '{search}' OR doctorid = '{search}'")
-
-    df = None
-
+    stmt_appointments = sa.text(f"SELECT *\nFROM {SCHEMA}.{TABLE}\nWHERE pxid = '{search}' OR doctorid = '{search}'")
+    df_appointments = None
     session = Session(engine)
-
     with session.begin():
         with engine.connect() as conn:
-            df = pd.read_sql_query(
-                stmt,
-                conn
-            )
-
+            df_appointments = pd.read_sql_query(stmt_appointments, conn)
+    
+    # Fetch unique doctor IDs
+    stmt_doctors = sa.text(f"SELECT DISTINCT doctorid FROM {SCHEMA}.{TABLE}")
+    df_doctors = pd.read_sql_query(stmt_doctors, engine)
+    
+    # Fetch unique patient IDs
+    stmt_patients = sa.text(f"SELECT DISTINCT pxid FROM {SCHEMA}.{TABLE}")
+    df_patients = pd.read_sql_query(stmt_patients, engine)
+    
+    # Fetch unique clinic IDs
+    stmt_clinics = sa.text(f"SELECT DISTINCT clinicid FROM {SCHEMA}.{TABLE}")
+    df_clinics = pd.read_sql_query(stmt_clinics, engine)
+    
     # Close the database connection
     engine.dispose()
 
-    return render_template('views/appointments.html',
-                           search_query=search if search else "",
-                           appointments=df
-                           )
+    return render_template('views/appointments.html', 
+                           search_query=search if search else "", 
+                           appointments=df_appointments,
+                           doctors=df_doctors,
+                           patients=df_patients,
+                           clinics=df_clinics)
 
 @app.route('/doctors')
 def doctors():
